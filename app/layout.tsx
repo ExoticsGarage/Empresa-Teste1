0bd458521a005e3f92d2dbca23be5e588b463e9c
ExoticsGarage <hthermolfiuwankdq@gmail.com>
2026-04-16 02:43:27 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,14 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Empresa Teste',
-  description: 'Página de teste publicada na Vercel',
-  icons: {
-    icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
-    ],
-    shortcut: '/icon.svg',
-    apple: '/apple-icon.png',
-  },
+  description: 'Página de teste',
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
